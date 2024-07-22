097ce312d6405c43710b7d6cd1f1ced7c0ab5192
吉村直人 <ahowateto123@gmail.com>
2024-07-23 02:33:53 +0900

    APIドメイン修正

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 
 function Index() {
-  const apiDomain = 'https://on7h3cdrrc.execute-api.ap-northeast-1.amazonaws.com/poc/rooms'
+  const apiDomain = 'https://on7h3cdrrc.execute-api.ap-northeast-1.amazonaws.com/poc'
   const getRoomsEndpoint = `${apiDomain}/rooms`;
 
   // 部屋取得APIのレスポンスを表示
