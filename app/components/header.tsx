9c3be95ce4d505b9af3f5ef297baaa8c4ab0e529
吉村直人 <ahowateto123@gmail.com>
2024-07-24 17:20:39 +0900

    不要そうな箇所削除

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -140,11 +140,6 @@ function Header() {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
             <Menu
               sx={{ mt: '45px' }}
               id="menu-appbar"
